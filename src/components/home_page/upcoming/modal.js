c523a2fdbc42da3f8eb4e7fc6fe044e9c5602c92
hadizz <hadizareoriginal@gmail.com>
2020-04-26 21:59:44 +0430

    Change edit todo modal view on mobile

--- src/components/home_page/upcoming/modal.js
+++ src/components/home_page/upcoming/modal.js
@@ -4,6 +4,8 @@ import './modal.css'
 import { tags } from "../../../data/dataArray"
 import DoneRoundedIcon from '@material-ui/icons/DoneRounded';
 import DoneAllRoundedIcon from '@material-ui/icons/DoneAllRounded';
+import ArrowBackIosRoundedIcon from '@material-ui/icons/ArrowBackIosRounded';
+
 export default function Modal(props) {
 
     const [task, setTask] = useState(props.preTask);
@@ -31,13 +33,13 @@ export default function Modal(props) {
                 return <center style={{ color: "green", marginTop: 7, marginBottom: -10 }}>{statMsg[1]}</center>
                 break;
             case 1:
-                return <span style={{ color: "blue", marginTop: 7 }}>{statMsg[1]}</span>
+                return <center style={{ color: "blue", marginTop: 7 }}>{statMsg[1]}</center>
                 break;
             case 2:
-                return <span style={{ color: "red", marginTop: 7 }}>{statMsg[1]}</span>
+                return <center style={{ color: "red", marginTop: 7 }}>{statMsg[1]}</center>
                 break;
             default:
-                return <span style={{ color: "purple", marginTop: 7 }}>!ارور ناشناخته</span>
+                return <center style={{ color: "purple", marginTop: 7 }}>!ارور ناشناخته</center>
                 break;
         }
     }
@@ -49,66 +51,62 @@ export default function Modal(props) {
                     <div id="modal-2" className="modal-container2">
 
                         {/* header */}
-                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: "space-between" }}>
-                            <Close
-                                onClick={() => {
-                                    props.close();
-                                    setStat(false);
-                                    setStatMsg([0, ""]);
-                                    setIsBtnClicked(false);
-                                }}
-                                style={{ color: "rgba(85, 85, 85)", cursor: "pointer" }}
-                            />
-                            <h3 style={{ cursor: "default" }}>ادیت تسک</h3>
+                        <div className="edit-modal-header">
+                            <div className="edit-modal-close">
+                                <Close
+                                    onClick={() => {
+                                        props.close();
+                                        setStat(false);
+                                        setStatMsg([0, ""]);
+                                        setIsBtnClicked(false);
+                                    }}
+                                    style={{ cursor: "pointer" }}
+                                />
+                            </div>
+                            <div className="edit-modal-arrow">
+                                <ArrowBackIosRoundedIcon
+                                    onClick={() => {
+                                        props.close();
+                                        setStat(false);
+                                        setStatMsg([0, ""]);
+                                        setIsBtnClicked(false);
+                                    }}
+                                    style={{ cursor: "pointer", fontSize: 35 }}
+                                />
+                            </div>
+                            <h3 style={{ cursor: "default" }}>ویرایش تسک</h3>
                         </div>
 
-                        {/* form */}
-                        <form style={{ display: 'flex', flexDirection: 'column', margin: "10px 0 auto 0" }}>
-                            <input
-                                style={{
-                                    direction: "rtl",
-                                    padding: "10px 10px 10px 10px",
-                                    margin: "5px 0 5px 0",
-                                    border: "0px",
-                                    borderRadius: "10px",
-                                    outline: "none",
-                                    fontSize: 16
-                                }}
-                                value={task}
-                                onChange={onEditTaskChange}
-                            ></input>
-                            <select style={{ direction: "rtl", outline: "none", borderRadius: 5, marginTop: 5, padding: "1px", fontSize: 16 }}>
-                                {tags.map((tag, index) => (
-                                    <option key={index} value={tag.name} selected={(tag.name === props.tag.name)}>{tag.name}</option>
-                                ))}
-                            </select>
-                            <button
-                                onClick={editTask}
-                                style={{
-                                    width: "auto",
-                                    backgroundColor: isBtnClicked ? "#006aff7c" : "#006aff",
-                                    color: "white",
-                                    fontWeight: 400,
-                                    fontSize: 16,
-                                    padding: "5px 7px 5px 7px",
-                                    margin: "12px 0 5px 0",
-                                    border: "0px",
-                                    borderRadius: "8px",
-                                    outline: "none",
-                                    cursor: isBtnClicked ? "not-allowed" : "pointer"
-                                }}
-                                disabled={isBtnClicked}
-                            >
-                                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: "center" }}>
-                                    {isBtnClicked ? <DoneAllRoundedIcon style={{ color: "white" }} /> : <DoneRoundedIcon style={{ color: "white" }} />}
-                                    <span style={{ marginTop: 3, marginLeft: 5 }}>حلـــه</span>
-                                </div>
-                            </button>
-                        </form>
+                        <div className="edit-modal-center">
 
-                        {/* status */}
-                        <ShowStatus />
+                            {/* form */}
+                            <form className="edit-modal-form">
+                                <input className="edit-modal-form-input" value={task} onChange={onEditTaskChange}></input>
+                                <select className="edit-modal-form-select">
+                                    {tags.map((tag, index) => (
+                                        <option key={index} value={tag.name} selected={(tag.name === props.tag.name)}>{tag.name}</option>
+                                    ))}
+                                </select>
+                                <button
+                                    onClick={editTask}
+                                    style={{
+                                        backgroundColor: isBtnClicked ? "#006aff7c" : "#006aff",
 
+                                        cursor: isBtnClicked ? "not-allowed" : "pointer"
+                                    }}
+                                    disabled={isBtnClicked}
+                                >
+                                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: "center" }}>
+                                        {isBtnClicked ? <DoneAllRoundedIcon style={{ color: "white" }} /> : <DoneRoundedIcon style={{ color: "white" }} />}
+                                        <span style={{ marginTop: 2, marginLeft: 5, fontSize: 18 }}>حلـــه</span>
+                                    </div>
+                                </button>
+                            </form>
+
+                            {/* status */}
+                            <ShowStatus />
+
+                        </div>
                     </div>
                 </div >
             }
