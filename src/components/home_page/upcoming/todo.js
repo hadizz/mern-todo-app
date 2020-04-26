594cf0f90c7e89d1ecb418caab166e70e88228c6
hadizz <hadizareoriginal@gmail.com>
2020-04-26 16:57:20 +0430

    Add animation to onclick on todo box

--- src/components/home_page/upcoming/todo.js
+++ src/components/home_page/upcoming/todo.js
@@ -8,13 +8,20 @@ import Modal from "./modal"
 export default function Todo({ details }) {
 
   const [show, setShow] = useState(false);
+  const [active, setActive] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const handleActive = () => {
+    console.log("clicked on todo box");
+    console.log("active : " + active);
+    setActive(!active);
+    console.log("active : " + active);
+  }
 
   return (
     <>
-      <div className="todoBox">
+      <div className="todoBox" onClick={handleActive} style={{ transform: active ? "scale(1.03)" : "scale(1)", boxShadow: active ? `0 0 20px -10px ${details.tag.color}` : "0 0 0 0 rgb(0,0,0)", borderColor: active ? details.tag.color : "#eff0f5" }}>
         <span className="todobox-line1">{details.description}</span>
         <div className="todobox-line2">
           <div className="tag-info">
