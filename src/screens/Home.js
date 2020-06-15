76281ff6214e82995c7aa4d768d1060af5f25c72
hadizz <hadizareoriginal@gmail.com>
2020-06-15 16:59:34 +0430

    add soundcloud song

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -9,11 +9,27 @@ import AddTodoBtn from "../components/home_page/addTodoButton/";
 const Home = () => {
   return (
     <div>
+      <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/741773860&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true">
+      </iframe>
+      <div className="style1">
+        <a href="https://soundcloud.com/elahe-ahmadipour"
+          title="elahe ahmadipour" target="_blank" className="style2"
+        >
+          elahe ahmadipour
+        </a>
+           ·
+        <a
+          href="https://soundcloud.com/elahe-ahmadipour/az-khone-javanan-parastoo-ahmadi"
+          title="Az khone javanan _ Parastoo Ahmadi" target="_blank" className="style3">
+          Az khone javanan _ Parastoo Ahmadi
+        </a>
+      </div>
       <Header />
       <Categories />
       <Upcoming />
       <AddTodoBtn />
-    </div>
+    </div >
   );
 }
 
